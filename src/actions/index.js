6327c8c854c6a186c94b33dc275235e209d3990e
dancaprimam <dancaprimaa@gmail.com>
2017-01-06 14:11:38 +0700

    Update weather list section

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
+import axios from 'axios';
+
 const API_KEY = 'd3bd590ff6984fbfec04dabe7e925c1f';
+const API_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city){
+    const url = `${API_URL}&q=${city},id`
+
+    const req = axios.get(url)
+  
+
+    return {
+        type: FETCH_WEATHER,
+        payload: req  
+    };
+}
